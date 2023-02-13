fe15efda69a06f6c0bafb330badfe62b4c7596c3
Mina Samy Ibrahim <71660135+ibrahimmina@users.noreply.github.com>
2023-02-13 12:48:35 +0200

    U21 - 13022023

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
-      <header>
+      <header class="mb-auto">
       <div>
         <h3 class="float-md-start mb-0">Cover</h3>
         <nav class="nav nav-masthead justify-content-center float-md-end">

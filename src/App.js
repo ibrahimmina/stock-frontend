39fe1049125dfad70c164dfa9c027d12279d9dd2
Mina Samy Ibrahim <71660135+ibrahimmina@users.noreply.github.com>
2023-02-13 13:39:07 +0200

    U23 - 13022023

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing"
 import Footer from "./components/layout/Footer"
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
@@ -10,7 +14,9 @@ class App extends Component {
       
         <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
           <Navbar />
-          <Landing />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
           <Footer />
 
         </div>

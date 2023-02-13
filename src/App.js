2d6e2b891cc6183f53305c96756cc1c2edcdc3a3
Mina Samy Ibrahim <71660135+ibrahimmina@users.noreply.github.com>
2023-02-13 12:03:49 +0200

    U16 - 13022023

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import Landing from "./components/layout/Landing"
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      
         <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
           <Navbar />
           <Landing />
 
         </div>
-      </div>
     );
   }
 }

9c4c74ace9c55ae4ed2108d414773b26d776e351
Mina Samy Ibrahim <71660135+ibrahimmina@users.noreply.github.com>
2023-02-13 10:55:03 +0200

    U9 - 13022023

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing"
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
           <Navbar />
+          <Landing />
 
         </div>
       </div>

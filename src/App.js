378ee311f8e78acd96810b6443536531d0e3609f
Mina Samy Ibrahim <71660135+ibrahimmina@users.noreply.github.com>
2023-02-13 09:49:52 +0200

    U4 - 13022023

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <Landing />
+        <h1>Hello</h1>
       </div>
     );
   }
